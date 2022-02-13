670cfc77c64f4887ab291c91f897090c037d22f2
azhari <azharisaputrasikumbang@gmail.com>
2022-02-14 01:17:46 +0700

    front page

---
+++ pages/pemilik/index.php
@@ -0,0 +1,3 @@
+<!-- @extends member.php -->
+<h3>Dashboard</h3>
+<p class="lead">Hi, <?= $_SESSION['username'] ?>. selamat datang, silahkan daftarkan jadwal anda <a href="./pemilik.php?page=pendaftaran/buat">disini</a>...</p>
---
+++ pages/petugas/index.php
@@ -0,0 +1,3 @@
+<!-- @extends admin.php -->
+<h3>Dashboard</h3>
+<p class="lead">Hi, <?= $_SESSION['username'] ?>. Siap bekerja hari ini ?</p>
